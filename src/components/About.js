5c0913f3b1bf0e515748a696c91c3eb3cf36274a
ankithda <shrimahimy@gmail.com>
2024-05-31 12:07:00 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h6>Your “About Us” page is an overview of your entire business containing valuable information, 
+        <tr>such as your business history, values, and products.
+          </tr>
+          <tr>
+             You can gain trust from your potential customers by adding reviews posted by your customers.
+          </tr>
+        <tr>It can also improve your e-commerce SEO performance by engaging more customers.
+          </tr>
+        </h6>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/AllProducts.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import { Box, TextField } from "@mui/material";
+import Alert from '@mui/material/Alert';
+
+function AllProducts() {
+  const [data,setProductdata]=useState([]);
+  const [search,setSerach]=useState(null);
+  useEffect(()=>{
+    axios.get(`https://dummyjson.com/products`)
+    .then((res)=>{
+      console.log(res.data.products );
+        setProductdata(res.data.products );
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+},[]);
+  let filtered=search?data.filter((item)=>
+  item.title.toLowerCase().includes(search.toLowerCase())
+  ):data;
+  console.log(filtered,"filterData")
+  return (
+    <div style={{ padding: 10 }}>
+      <Box
+        sx={{
+          p: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextField label={"Search products here"}
+        onChange={((e)=>setSerach(e.target.value))} />
+      </Box>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: 3,
+          "& > :not(style)": {},
+        }}
+        >
+      {filtered.length > 0?
+        filtered.map((product)=>(
+          <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="200"
+            width="200"
+            image={product.thumbnail}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              title
+            </Typography>
+            <Typography gutterBottom variant="body2" color="text.secondary">
+              category
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              description
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Typography variant="h5">₹price</Typography>
+
+            <Typography
+              variant="h5"
+              style={{ textAlign: "end", width: "100%" }}
+            >
+              brand
+            </Typography>
+          </CardActions>
+        </Card>
+        ))
+      :
+      <Box>
+        <Alert severity="warning">no search found</Alert>
+        </Box>
+      } 
+      </Grid>
+    </div>
+  );
+}
+
+export default AllProducts;
